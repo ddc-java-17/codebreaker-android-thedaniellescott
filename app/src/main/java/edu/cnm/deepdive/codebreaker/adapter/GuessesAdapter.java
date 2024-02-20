f23050c112236499b2f9c5c7ce6245713f887b5e
DanielleScott <dmyers28@cnm.edu>
2024-02-20 10:56:35 -0700

    Modified layouts and GuessAdapter to allow for color swatches to show instead of color initials. In progress.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/GuessesAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/GuessesAdapter.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.codebreaker.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +12,39 @@ import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.ItemGuessesBinding;
 import edu.cnm.deepdive.codebreaker.model.Guess;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class GuessesAdapter extends ArrayAdapter<Guess> {
 
-  // TODO: 2/19/2024 Declare additional fields for Map<Character, String> for color names,
-  //  Map<Character, Integer> for color values, and the drawable.
   private final LayoutInflater inflater;
+  private final Map<Integer, String> colorNameLookup;
+  private final Map<Integer, Integer> colorValueLookup;
+
 
   public GuessesAdapter(@NonNull Context context, @NonNull List<Guess> guesses) {
     super(context, R.layout.item_guesses, guesses);
     inflater = LayoutInflater.from(context);
-    // TODO: 2/19/2024 Create and populate maps, using string-array and integer-array resources.
+    Resources res = context.getResources();
+    int[] colorValues = res.getIntArray(R.array.color_values);
+    String[] colorNames = res.getStringArray(R.array.color_names);
+    colorNameLookup = Stream.of(colorNames)
+        .collect(
+            Collectors.toMap(
+                (name) -> name.codePointAt(0),
+                (name) -> name
+            )
+        );
+    colorValueLookup = IntStream.range(0, colorNames.length)
+        .boxed()
+        .collect(
+            Collectors.toMap(
+                (pos) -> colorNames[pos].codePointAt(0),
+                (pos) -> colorValues[pos]
+            )
+        );
   }
 
   @NonNull
@@ -30,7 +53,9 @@ public class GuessesAdapter extends ArrayAdapter<Guess> {
     ItemGuessesBinding binding = (convertView == null)
         ? ItemGuessesBinding.inflate(inflater, parent, false)
         : ItemGuessesBinding.bind(convertView);
+    binding.guess.removeAllViews();
     Guess guess = getItem(position);
+
     // TODO: 2/19/2024 Iterate over characters of guess, using each to look up the color name and
     //  value from the maps above. Then, set tint of drawable to color value and add to container.
     binding.guess.setText(guess.getContent());
