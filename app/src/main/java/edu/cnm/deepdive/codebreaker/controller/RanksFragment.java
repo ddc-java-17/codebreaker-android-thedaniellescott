da5f82dffbc577e6ece7b012482358adf1486923
DanielleScott <dmyers28@cnm.edu>
2024-03-11 10:37:02 -0600

    bottom navigation in progress.

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.FragmentRanksBinding;
+
+public class RanksFragment extends Fragment {
+
+  private FragmentRanksBinding binding;
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    binding = FragmentRanksBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onDestroyView() {
+    binding = null;
+    super.onDestroyView();
+  }
+
+}
