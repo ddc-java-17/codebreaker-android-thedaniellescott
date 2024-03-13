a977463621a55d5636f900dfb63facc05e31480b
DanielleScott <dmyers28@cnm.edu>
2024-03-13 10:32:50 -0600

    Fixed and implemented drawer navigation

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -1,13 +1,9 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
 import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
+import android.os.Bundle;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,9 +12,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.material.navigation.NavigationView;
 import dagger.hilt.android.AndroidEntryPoint;
-import edu.cnm.deepdive.codebreaker.MainNavigationMapDirections;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.ActivityMainBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.LoginViewModel;
@@ -29,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
   private ActivityMainBinding binding;
   private LoginViewModel loginViewModel;
   private NavController navController;
-
+  private AppBarConfiguration appBarConfig;
+  private DrawerLayout drawer;
+  // TODO: 3/13/2024 Add OnBackPressedCallback.
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +39,15 @@ public class MainActivity extends AppCompatActivity {
     loginViewModel
         .getThrowable()
         .observe(this, this::handleThrowable);
-    setContentView(binding.getRoot());
+    drawer = binding.getRoot();
+    setContentView(drawer);
     setupNavigation();
-    setupDrawer();
   }
 
   @Override
   public boolean onSupportNavigateUp() {
-    getOnBackPressedDispatcher().onBackPressed();
-    return true;
-  }
-
-  @Override
-  public void onBackPressed() {
-    DrawerLayout drawer = binding.getRoot();
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-    } else {
-      super.onBackPressed();
-    }
+    return NavigationUI.navigateUp(navController, binding.getRoot())
+        || super.onSupportNavigateUp();
   }
 
   private void handleAccount(GoogleSignInAccount account) {
@@ -77,26 +63,24 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void setupNavigation() {
-    AppBarConfiguration config = new AppBarConfiguration.Builder(
-        R.id.game_fragment, R.id.scores_fragment, R.id.ranks_fragment)
-        .setFallbackOnNavigateUpListener(this::onSupportNavigateUp)
-        .build();
     //noinspection DataFlowIssue
     navController = ((NavHostFragment) getSupportFragmentManager()
         .findFragmentById(R.id.nav_host_fragment))
         .getNavController();
-//    setSupportActionBar(binding.appBarLayout.toolbar);
-//    NavigationUI.setupActionBarWithNavController(this, navController, config);
-    NavigationUI.setupWithNavController(binding.appBarLayout.toolbar, navController, config);
+    appBarConfig = new AppBarConfiguration.Builder(
+        R.id.game_fragment, R.id.scores_fragment, R.id.ranks_fragment, R.id.settings_fragment)
+        .setOpenableLayout(drawer)
+        .build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
+    NavigationUI.setupWithNavController(binding.navigator, navController);
+    binding.navigator.getMenu()
+        .findItem(R.id.sign_out)
+        .setOnMenuItemClickListener(item -> {
+          loginViewModel.signOut();
+          return true;
+        });
   }
 
-  private void setupDrawer() {
-    DrawerLayout drawer = binding.getRoot();
-    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
-        binding.appBarLayout.toolbar, R.string.nav_open, R.string.nav_close);
-    drawer.addDrawerListener(toggle);
-    toggle.syncState();
 
-  }
 
 }
